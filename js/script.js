83377d6b6dba07263c6e0b66a457acc0dd0a8e69
Erin Lechler <erinlechler@gmail.com>
2020-06-27 14:37:25 -0500

    search js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
 //http://sachinchoolur.github.io/lightGallery/
 
+//====================== calling the light gallery plug in ======================================
     $(document).ready(function() {
         $("#lightgallery").lightGallery({
             counter: false
         }); 
-        
     });
+//============================== search box ==============================       
+$("#search").keyup(function() {
+        let searchInput = $("#search").val().toLowerCase();
+            $('#lightgallery > a').hide().filter(function() {
+                return $(this).attr('title').toLowerCase().includes(searchInput);
+                }).show();
+  });
+  
